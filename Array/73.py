decd43c37c98d74dff3dd2904d034338187b328d
Vishnu Varshath Harishankar <varshath@Vishnus-MBP.fios-router.home>
2018-11-13 18:15:39 -0500

    Set Matrix Zeros

---
+++ Array/73.py
@@ -0,0 +1,37 @@
+class Solution:
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        firstRowZero=False
+        firstColumnZero=False
+        for i in range(len(matrix)):
+            if matrix[i][0]==0:
+                firstColumnZero=True
+        for j in range(len(matrix[0])):
+            if matrix[0][j]==0:
+                firstRowZero=True
+        for i in range(0,len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    matrix[i][0]=0
+                    matrix[0][j]=0
+        for i in range(1,len(matrix)):
+            if matrix[i][0]==0:
+                for j in range(1,len(matrix[i])):
+                    matrix[i][j]=0
+        for j in range(1,len(matrix[0])):
+            if matrix[0][j]==0:
+                for i in range(1,len(matrix)):
+                    matrix[i][j]=0
+        if firstRowZero:
+            for j in range(len(matrix[0])):
+                matrix[0][j]=0
+        if firstColumnZero:
+            for i in range(len(matrix)):
+                matrix[i][0]=0
+        
+        
+        
+        
